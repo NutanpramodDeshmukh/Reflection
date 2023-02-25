7207921738f2ed7cc524a661e7e88df9555c1fa1
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-25 21:18:18 +0530

    Added: initial message

---
+++ Reflection/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object b = MoodAnalyseFactory.CreaateMoodAnalyse("_Reflection.MoodAnalyseFactory", "MoodAnalyseFactory");
+            Console.WriteLine(b.GetType().Name);
+        }
+    }
+}
